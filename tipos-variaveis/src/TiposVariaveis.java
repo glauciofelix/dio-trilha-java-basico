b7158c87e127eb115ff690caabe4fd21891d2a6d
glauciofelix <glaucio.fn@gmail.com>
2023-09-28 10:17:33 -0300

    commit inicial

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,20 @@
+public class TiposVariaveis {
+	public static void main(String[] args) {
+		double salarioMinimo = 2500;
+		
+		short numeroCurto = 1;
+		int numeroNormal = numeroCurto;
+		short numeroCurto2 = (short) numeroNormal;
+		
+		// variaveis
+		int numero = 5;
+		
+		numero = 10;
+		
+		System.out.println(numero);
+		
+		// constante
+		final double VALOR_DE_PI = 3.14D;
+		
+	}
+}
