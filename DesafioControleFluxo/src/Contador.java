ca085079afaf7378e196c0664287ea1c70376607
glauciofelix <glaucio.fn@gmail.com>
2023-09-29 21:31:14 -0300

    commit atualizações6

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,37 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in).useLocale(Locale.US);
+		
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			//chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException e) {
+			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+			System.err.println("O segundo parâmetro deve ser maior que o primeiro");
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		if (parametroUm >= parametroDois) {
+			throw new ParametrosInvalidosException();
+		}else {
+			int contagem = parametroDois - parametroUm;
+			//realizar o for para imprimir os números com base na variável contagem
+			for (contagem = parametroUm; contagem <= parametroDois; contagem++) {
+				System.out.println(contagem);
+		}
+		
+		}
+	}
+}
